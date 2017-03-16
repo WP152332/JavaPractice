3b2d1946400d98af554ccfc6694ebd7723935d8d
JunYongChoi <JunYongChoi@DESKTOP-SK77K1S>
2017-03-16 11:19:02 +0900

    게임 제작

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -30,12 +30,13 @@ public class Loop {
 		Scanner scanner = new Scanner(System.in);
 		int choice;
 		while(true) {
-			System.out.println("******************");
+			System.out.println("--------------------");
 			System.out.println("<< 게임 메뉴 >>");
 			System.out.println("1. 공격력 증가");
 			System.out.println("2. 공격력 감소");
 			System.out.println("3. 캐릭터 설정");
 			System.out.println("9. 종료");
+			System.out.println("--------------------");
 			System.out.println("메뉴 입력 => ");
 			choice = scanner.nextInt();
 			if(choice == 1) {
