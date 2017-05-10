67c10676886ead7db26472c5080ca2168f61d4a3
JunYongChoi <JunYongChoi@JY>
2017-05-10 12:33:18 +0900

    personteset

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *		|_ PersonTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 26.
+ * </pre>
+ *
+ * @author			:	JunYongChoi
+ * @version		:	1.0
+ */
+public class PersonTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person[] p = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		for(Person pp : p) {
+			greeting(pp);
+		}
+	}
+	
+	private static void greeting(Person p) {
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+}
