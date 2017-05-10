14f7e46b1a6d8138fa4d7962725a5c6742a0a9bf
JunYongChoi <JunYongChoi@JY>
2017-05-10 12:35:05 +0900

    check

--- JavaPractice/src/org/dimigo/inheritance/PersonTest.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -26,7 +26,6 @@ public class PersonTest {
 		Korean k = new Korean("홍길동");
 		Japanese j = new Japanese("다나카");
 		Chinese c = new Chinese("왕밍");
-		
 		System.out.println(p);
 		System.out.println(k);
 		System.out.println(j);
